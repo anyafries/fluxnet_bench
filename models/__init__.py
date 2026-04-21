c7e9c267029b9a9a72f1dd473bc93fcccd2a1fa2
anyafries <anfries@ethz.ch>
2026-04-21 12:11:01 +0200

    move package loading, add robust lr

--- models/__init__.py
+++ models/__init__.py
@@ -15,14 +15,6 @@ Available models:
 
 import itertools
 
-from sklearn.linear_model import LinearRegression, Ridge
-from xgboost import XGBRegressor
-
-from .maxrm_rf import MaxRM_RF
-from .mlp import MLP
-from .gdro import GroupDRO
-from .coral import CORAL, MMD
-
 from utils.utils import setup_logging
 
 logger = setup_logging(__name__)
@@ -46,23 +38,45 @@ def get_model(model_name, params=None):
     # params = get_default_params(model_name)
 
     if model_name == 'xgb':
+        from xgboost import XGBRegressor
         return XGBRegressor(**params)
+    
     elif model_name == 'lr':
+        from sklearn.linear_model import LinearRegression
         return LinearRegression()
+    
+    elif model_name == 'robust-lr':
+        from sklearn.linear_model import HuberRegressor
+        return HuberRegressor(**params)
+    
     elif model_name == 'ridge':
+        from sklearn.linear_model import Ridge
         return Ridge(**params)
+    
     elif model_name == 'mlp':
+        from .mlp import MLP
         return MLP(**params)
+    
     elif model_name == 'gdro':
+        from .gdro import GroupDRO
         return GroupDRO(**params)
+    
     elif model_name == 'coral':
+        from .coral import CORAL
         return CORAL(**params)
+    
     elif model_name == 'mmd':
+        from .coral import MMD
         return MMD(**params)
-    elif model_name == 'maxrm_mse':
+    
+    elif model_name == 'maxrm-mse':
+        from .maxrm_rf import MaxRM_RF
         return MaxRM_RF(risk='mse', **params)
-    elif model_name == 'maxrm_regret':
+    
+    elif model_name == 'maxrm-regret':
+        from .maxrm_rf import MaxRM_RF
         return MaxRM_RF(risk='regret', **params)
+    
     else:
         raise NotImplementedError(
             f"Model `{model_name}` not implemented. "
@@ -70,72 +84,72 @@ def get_model(model_name, params=None):
         )
 
 
-def get_default_params(model_name):
-    """
-    Returns default parameters for the specified model.
+# def get_default_params(model_name):
+#     """
+#     Returns default parameters for the specified model.
 
-    Args:
-        model_name (str): The name of the model.
+#     Args:
+#         model_name (str): The name of the model.
 
-    Returns:
-        dict: Default parameters for the model
-    """
-    params = {}
-    if model_name == 'xgb':
-        params = {
-            'objective': 'reg:squarederror',
-            'n_estimators': 100,
-            'max_depth': 5,
-            'learning_rate': 0.1,
-            'early_stopping_rounds': 10,
-        }
-    elif model_name in ['maxrm_mse', 'maxrm_regret']:
-        params = {
-            'n_estimators': 100,
-            'min_samples_leaf': 30,
-        }
-    elif model_name == 'mlp':
-        params = {
-            'hidden_dims': [128, 64],
-            'dropout': 0.1,
-            'lr': 1e-3,
-            'n_epochs': 500,
-            'batch_size': 256,
-            'early_stopping_rounds': 10,
-        }
-    elif model_name == 'gdro':
-        params = {
-            'hidden_dims': [128, 64],
-            'dropout': 0.1,
-            'lr': 1e-3,
-            'n_epochs': 100,
-            'batch_size': 256,
-            'group_weight_step': 0.01,
-            'early_stopping_rounds': 10,
-        }
-    elif model_name == 'coral':
-        params = {
-            'hidden_dims': [128, 64],
-            'dropout': 0.1,
-            'lr': 1e-3,
-            'n_epochs': 500,
-            'batch_size': 1024,
-            'early_stopping_rounds': 10,
-            'coral_lambda': 1.0,
-            'num_coral_pairs': 10
-        }
-    elif model_name == 'mmd':
-        params = {
-            'hidden_dims': [128, 64],
-            'dropout': 0.1,
-            'lr': 1e-3,
-            'n_epochs': 500,
-            'batch_size': 1024,
-            'early_stopping_rounds': 10,
-            'mmd_gamma': 1.0,
-            'num_mmd_pairs': 5,
-        }
-    return params
+#     Returns:
+#         dict: Default parameters for the model
+#     """
+#     params = {}
+#     if model_name == 'xgb':
+#         params = {
+#             'objective': 'reg:squarederror',
+#             'n_estimators': 100,
+#             'max_depth': 5,
+#             'learning_rate': 0.1,
+#             'early_stopping_rounds': 10,
+#         }
+#     elif model_name in ['maxrm_mse', 'maxrm_regret']:
+#         params = {
+#             'n_estimators': 100,
+#             'min_samples_leaf': 30,
+#         }
+#     elif model_name == 'mlp':
+#         params = {
+#             'hidden_dims': [128, 64],
+#             'dropout': 0.1,
+#             'lr': 1e-3,
+#             'n_epochs': 500,
+#             'batch_size': 256,
+#             'early_stopping_rounds': 10,
+#         }
+#     elif model_name == 'gdro':
+#         params = {
+#             'hidden_dims': [128, 64],
+#             'dropout': 0.1,
+#             'lr': 1e-3,
+#             'n_epochs': 100,
+#             'batch_size': 256,
+#             'group_weight_step': 0.01,
+#             'early_stopping_rounds': 10,
+#         }
+#     elif model_name == 'coral':
+#         params = {
+#             'hidden_dims': [128, 64],
+#             'dropout': 0.1,
+#             'lr': 1e-3,
+#             'n_epochs': 500,
+#             'batch_size': 1024,
+#             'early_stopping_rounds': 10,
+#             'coral_lambda': 1.0,
+#             'num_coral_pairs': 10
+#         }
+#     elif model_name == 'mmd':
+#         params = {
+#             'hidden_dims': [128, 64],
+#             'dropout': 0.1,
+#             'lr': 1e-3,
+#             'n_epochs': 500,
+#             'batch_size': 1024,
+#             'early_stopping_rounds': 10,
+#             'mmd_gamma': 1.0,
+#             'num_mmd_pairs': 5,
+#         }
+#     return params
 
 
 def get_param_grid(model_name):
@@ -145,8 +159,23 @@ def get_param_grid(model_name):
     """
     grid = []
     
-    if model_name in ['lr', 'ridge']:
+    if model_name == 'lr':
         grid = [{}]
+
+    elif model_name == 'ridge':
+        options = {
+            'alpha': [0.1, 1.0, 10.0]
+        }
+        keys, values = zip(*options.items())
+        grid = [dict(zip(keys, v)) for v in itertools.product(*values)]
+
+    elif model_name == 'robust-lr':
+        options = {
+            'alpha': [0.1, 1.0, 10.0],
+            'epsilon': [1.35, 1.5]
+        }
+        keys, values = zip(*options.items())
+        grid = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
     elif model_name in ['maxrm_mse', 'maxrm_regret']:
         options = {
@@ -174,7 +203,7 @@ def get_param_grid(model_name):
             'hidden_dims': [[128, 64], [256, 128]],
             'lr': [1e-3, 1e-4],
             'n_epochs': [100],
-            'batch_size': [256]
+            'batch_size': [1024, 2048]
         }
         
         # Add model-specific hyperparams
