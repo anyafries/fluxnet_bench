fbaf7fdf5eb724600ac382b491e9b078ea0b50be
anyafries <anfries@ethz.ch>
2026-05-03 20:03:55 +0200

    more plots

--- paper_experiments/paper_plots.py
+++ paper_experiments/paper_plots.py
@@ -1,11 +1,5 @@
 """
-Script to load and compare results from multiple experiments.
-
-This script loads pre-computed metrics if available, or computes them from
-predictions. Results are saved to results/metrics/ for subsequent runs.
-
-Usage:
-    python eval.py --setting spatial-easy --target GPP
+Script to plot results from multiple experiments for the paper.
 """
 
 import matplotlib.pyplot as plt
@@ -67,6 +61,116 @@ if __name__ == "__main__":
     results['scale'] = results['scale'].replace({'spatial': 'site-mean'})
 
     plt.style.use('/u/anfries/fluxnet_bench/utils/neurips.mplstyle')
+
+    # TRYING TO FIGURE OUT HOW TO PLOT SATURATION OF FLUXES
+    agg = results.groupby(['target', 'setting', 'scale', 'model'])['rmse'].agg(['median', lambda x: x.quantile(0.9)]).reset_index()
+    agg.rename(columns={'<lambda_0>': 'q90'}, inplace=True)
+    agg_best = (
+        agg
+        .groupby(['target', 'setting', 'scale'])
+        .agg({'q90': 'min', 'median': 'min'})
+        .reset_index()
+    )
+    agg = agg.merge(agg_best[['target', 'setting', 'scale', 'q90', 'median']], 
+                    on=['target', 'setting', 'scale'], suffixes=('', '_best'))
+    agg['relative_q90'] = agg['q90'] / agg['q90_best']
+    agg['relative_median'] = agg['median'] / agg['median_best']
+
+    fig, axes = plt.subplots(2, 3, figsize=(3.5, 2), sharey=True, sharex=True,
+                             gridspec_kw={'hspace': 0.3, 'wspace': 0.2})
+    axes = axes.flatten()
+    for i, scale in enumerate(SCALES):
+        ax = axes[i]
+        df_i = agg[agg['scale'] == scale]
+        df_i.groupby('target')['relative_q90'].rank(method='min')
+        df_i['rank'] = df_i.groupby(['target', 'setting'])['relative_q90'].rank()
+        df_i = df_i[df_i['rank'] == 4]
+        sns.lineplot(
+            data=df_i, x='target', y='relative_q90', hue='setting', ax=ax,
+            linewidth=1, markers=True, marker='o', markeredgecolor='none',
+            markersize=2,
+        )
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+        ax.set_title(scale)
+        ax.set_ylim(1.0, 1.3)
+        ax.legend().remove()
+        if i == 4:
+            ax.legend()
+
+    # add a ylabel in the middle of the left side
+    fig.text(0.01, 0.5, 'range of top-4 baselines (relative to best)', 
+             va='center', rotation='vertical')
+    plt.tight_layout()
+    plt.savefig(f'{PLOTS_DIR}/boxplots_by_scale_all_settings.png', bbox_inches='tight', dpi=300)
+    print(f"Boxplots by scale for all settings saved to {PLOTS_DIR}/boxplots_by_scale_all_settings.png")
+    raise
+
+
+
+
+
+    # KEEP ALL MODELS, BUT ONLY TA40 SETTING TO COMPARE SCALES
+    agg = agg[agg['setting'] == 'TA40']
+
+    COL_TO_PLOT = 'relative_q90'
+    fig, axes = plt.subplots(2, 3, figsize=(5.5, 2.5), sharey=True)
+    axes = axes.flatten()
+    for i, scale in enumerate(SCALES):
+        ax = axes[i]
+        df_i = agg[agg['scale'] == scale]
+        sns.lineplot(
+            data=df_i,
+            x='target',
+            y=COL_TO_PLOT,
+            hue='model',
+            palette=MODEL_COLORS,
+            ax=ax,
+            linewidth=1,
+            markers=True,
+            marker='o',
+            markeredgecolor='none',
+            markersize=2,
+        )
+        ax.set_xlabel('')
+        ylabel = 'Relative 90th\nPercentile of RMSE' if COL_TO_PLOT == 'relative_q90' else 'Relative Median RMSE'
+        ax.set_ylabel(ylabel)
+
+        # make a shaded area for the top 4 models
+        df_i['model_rank'] = df_i.groupby('target')[COL_TO_PLOT].rank(method='min')
+        top4_ranges = df_i[df_i['model_rank'] <= 4].groupby('target')[COL_TO_PLOT].agg(['max']).reset_index()
+        top4_ranges['min'] = 1.0
+        ax.fill_between(
+            x=top4_ranges['target'],
+            y1=top4_ranges['min'],
+            y2=top4_ranges['max'],
+            color="#DBFCAE",
+            alpha=0.5,
+        )
+        ax.set_title(scale)
+        ax.set_ylim(1.0, 1.4)
+        ax.legend().remove()
+
+    # add a legend at the bottom center
+    handles, labels = ax.get_legend_handles_labels()
+    fig.legend(
+        handles, labels,
+        title="Model",
+        loc='lower center',
+        ncol=4,
+        frameon=False,
+        handlelength=0.6,
+        handleheight=0.4,
+        handletextpad=0.3,
+        labelspacing=0.2,
+        borderpad=0.2,
+    )
+    plt.tight_layout()
+    plt.savefig(f'{PLOTS_DIR}/boxplots_by_scale.png', bbox_inches='tight', dpi=300)
+    print(f"Boxplots by scale saved to {PLOTS_DIR}/boxplots_by_scale.png")
+
+    raise
+    
 
     flux_saturation_results = []
     for target in ['GPP', 'ET', 'NEE']:
